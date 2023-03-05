eb84e09139c64f4f914b1e1bd83825471600c2bb
juanhernandezdeveloper <jualherac@gmail.com>
2023-03-05 00:50:09 -0600

    Tables Customers and Addresses working in first version

---
+++ Controllers/AddressesController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SD_330_F22SD_Labs.Data;
+using SD_330_F22SD_Labs.Models;
+
+namespace SD_330_F22SD_Labs.Controllers
+{
+    public class AddressesController : Controller
+    {
+        private readonly Lab1Context _context;
+
+        public AddressesController(Lab1Context context)
+        {
+            _context = context;
+        }
+
+        //GET: Addresses
+
+        //public IActionResult Index()
+        //{
+        //    List<Address> addresses = _context.Address.ToList();
+
+        //    return View(addresses);
+        //}
+
+        public IActionResult index()
+        {
+            List<Address> addresses = _context.Address.Include(a =>
+            a.Customer
+            ).ToList();
+            return View(addresses);
+        }
+
+
+
+        //public async Task<IActionResult> Index()
+        //{
+        //    var lab1Context = _context.Address.Include(a => a.Customer);
+        //    return View(await lab1Context.ToListAsync());
+        //}
+
+
+
+        // GET: Addresses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Address == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address
+                .Include(a => a.Customer)
+                .FirstOrDefaultAsync(m => m.AddressId == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // GET: Addresses/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customer, "CustomerId", "CustomerId");
+            return View();
+        }
+
+        // POST: Addresses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AddressId,AddressLine1,AddressLine2,City,StateProvince,CountryRegion,PostalCode,CustomerId")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(address);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customer, "CustomerId", "CustomerId", address.CustomerId);
+            return View(address);
+        }
+
+        // GET: Addresses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Address == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customer, "CustomerId", "CustomerId", address.CustomerId);
+            return View(address);
+        }
+
+        // POST: Addresses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AddressId,AddressLine1,AddressLine2,City,StateProvince,CountryRegion,PostalCode,CustomerId")] Address address)
+        {
+            if (id != address.AddressId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(address);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddressExists(address.AddressId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customer, "CustomerId", "CustomerId", address.CustomerId);
+            return View(address);
+        }
+
+        // GET: Addresses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Address == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address
+                .Include(a => a.Customer)
+                .FirstOrDefaultAsync(m => m.AddressId == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // POST: Addresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Address == null)
+            {
+                return Problem("Entity set 'Lab1Context.Address'  is null.");
+            }
+            var address = await _context.Address.FindAsync(id);
+            if (address != null)
+            {
+                _context.Address.Remove(address);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AddressExists(int id)
+        {
+          return _context.Address.Any(e => e.AddressId == id);
+        }
+    }
+}
---
+++ Data/Lab1Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SD_330_F22SD_Labs.Models;
+
+namespace SD_330_F22SD_Labs.Data
+{
+    public class Lab1Context : DbContext
+    {
+        public Lab1Context (DbContextOptions<Lab1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customer { get; set; } = default!;
+
+        public DbSet<Address> Address { get; set; } = default!;
+    }
+}
