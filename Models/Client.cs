aec8809e9079b3ff6fe675ecfda9e331d2bb6537
juanhernandezdeveloper <jualherac@gmail.com>
2023-03-16 10:33:07 -0500

    First commit

---
+++ Models/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace SD_330_F22SD_Labs.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,18 @@
+﻿namespace SD_330_F22SD_Labs.Models
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+
+        public int Capacity { get; set; }
+
+        public Section Section { get; set; }
+    }
+
+    public enum Section
+    {
+        First,
+        Second,
+        Third
+    }
+}
